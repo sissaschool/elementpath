bb8ebe2cb7e0c35958a44ee1cf819935ba06dcd9
Davide Brunato <brunato@sissa.it>
2019-01-21 07:13:19 +0100

    Bugfix release v1.1.2
      - Fix equality operators for AbstractDateTime

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ author = 'Davide Brunato'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.1.2'
 
 
 # -- General configuration ---------------------------------------------------
