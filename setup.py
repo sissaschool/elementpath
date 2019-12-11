af3a65fb9d777a653e92403bb6383af0e677b824
Davide Brunato <brunato@sissa.it>
2019-12-11 09:46:24 +0100

    Set 'attribute' token for XPath 2 with multi-label ('kind test', 'axis')

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.rst") as readme:
 
 setup(
     name='elementpath',
-    version='1.3.2',
+    version='1.3.3',
     packages=['elementpath'],
     author='Davide Brunato',
     author_email='brunato@sissa.it',
