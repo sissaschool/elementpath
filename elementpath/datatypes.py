92457e92bb274c670344b9f913421de0d327d275
Davide Brunato <brunato@sissa.it>
2020-02-25 18:51:18 +0100

    Cover and fix XSD datatypes

--- elementpath/datatypes.py
+++ elementpath/datatypes.py
@@ -124,8 +124,11 @@ class Timezone(datetime.tzinfo):
     def fromduration(cls, duration):
         return cls(datetime.timedelta(seconds=int(duration.seconds)))
 
-    def __getinitargs__(self):
+    def __getnewargs__(self):
         return self.offset,
+
+    def __getnewargs_ex__(self):
+        return (self.offset,), {}
 
     def __hash__(self):
         return hash(self.offset)
@@ -231,10 +234,7 @@ class AbstractDateTime(metaclass=ABCMeta):
 
     @abstractmethod
     def __str__(self):
-        pass
-
-    def __unicode__(self):
-        return str(self)
+        raise NotImplementedError()
 
     @property
     def year(self):
@@ -251,7 +251,7 @@ class AbstractDateTime(metaclass=ABCMeta):
         if -9999 <= year < -1:
             return '{:05}'.format(year if self.version == '1.0' else year + 1)
         elif year == -1:
-            return '{:04}'.format(year if self.version == '1.0' else year + 1)
+            return '-0001' if self.version == '1.0' else '0000'
         elif 0 <= year <= 9999:
             return '{:04}'.format(year)
         else:
@@ -366,20 +366,20 @@ class AbstractDateTime(metaclass=ABCMeta):
     def __eq__(self, other):
         try:
             return operator.eq(*self._get_operands(other)) and self.year == other.year
-        except ElementPathTypeError:
+        except TypeError:
             return False
 
     def __ne__(self, other):
         try:
             return operator.ne(*self._get_operands(other)) or self.year != other.year
-        except ElementPathTypeError:
+        except TypeError:
             return True
 
 
 class OrderedDateTime(AbstractDateTime):
     @abstractmethod
     def __str__(self):
-        pass
+        raise NotImplementedError()
 
     @classmethod
     def fromdelta(cls, delta, adjust_timezone=False):
@@ -388,7 +388,7 @@ class OrderedDateTime(AbstractDateTime):
         0001-01-01T00:00:00 CE. In case of a date the time part is not counted.
 
         :param delta: a datetime.timedelta instance.
-        :param adjust_timezone: if `True` adjust the timezone of Date objects \
+        :param adjust_timezone: if `True` adjusts the timezone of Date objects \
         with eventually present hours and minutes.
         """
         try:
@@ -438,11 +438,8 @@ class OrderedDateTime(AbstractDateTime):
 
         if issubclass(cls, Date10):
             if adjust_timezone and dt.hour or dt.minute:
-                if dt.tzinfo is None:
-                    hour, minute = dt.hour, dt.minute
-                else:
-                    hour = dt.hour - dt.tzinfo.offset.hours
-                    minute = dt.minute - dt.tzinfo.offset.minutes
+                assert dt.tzinfo is None
+                hour, minute = dt.hour, dt.minute
 
                 if hour < 14 or hour == 14 and minute == 0:
                     tz = Timezone(datetime.timedelta(hours=-hour, minutes=-minute))
@@ -489,9 +486,8 @@ class OrderedDateTime(AbstractDateTime):
                 seconds = delta.total_seconds()
                 if self.bce ^ (seconds > 0):
                     raise
-                dt_seconds = abs((self._dt - datetime.datetime(1, 1, 1)).total_seconds() - seconds)
-                dt = datetime.datetime(1, 1, 1) + datetime.timedelta(seconds=dt_seconds)
-                bce = not self.bce
+                dt_seconds = seconds - (self._dt - datetime.datetime(1, 1, 1)).total_seconds()
+                return type(self).fromdelta(datetime.timedelta(seconds=dt_seconds))
             else:
                 bce = self.bce
 
@@ -509,7 +505,7 @@ class OrderedDateTime(AbstractDateTime):
             dt = self._dt.replace(year=year, month=month, day=adjust_day(year, month, self._dt.day))
 
         else:
-            raise ElementPathTypeError("wrong type %r for operand %r." % (type(other), other))
+            raise ElementPathTypeError("wrong type %r for operand %r" % (type(other), other))
 
         kwargs = {k: getattr(dt, k) for k in self._pattern.groupindex.keys()}
         if bce:
