7b4583da18f612895dbb422d39a7f88b93135da8
Davide Brunato <brunato@sissa.it>
2020-03-13 09:09:16 +0100

    Add xs:untypedAtomic argument type to XPath 2 constructors

--- tests/test_xpath2_constructors.py
+++ tests/test_xpath2_constructors.py
@@ -51,6 +51,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
     def test_token_constructor(self):
         self.check_value('xs:token(" hello  world ")', "hello world")
         self.check_value('xs:token("hello\t world\n")', "hello world")
+        self.check_value('xs:token(xs:untypedAtomic("hello\t world\n"))', "hello world")
         self.check_value('xs:token(())', [])
 
         root = self.etree.XML('<root a=" hello  world "/>')
@@ -59,6 +60,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
 
     def test_language_constructor(self):
         self.check_value('xs:language(" en ")', "en")
+        self.check_value('xs:language(xs:untypedAtomic(" en "))', "en")
         self.check_value('xs:language(" en-GB ")', "en-GB")
         self.check_value('xs:language("it-IT")', "it-IT")
         self.check_value('xs:language("i-klingon")', 'i-klingon')  # IANA-registered language
@@ -72,6 +74,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
 
     def test_nmtoken_constructor(self):
         self.check_value('xs:NMTOKEN(" :menù.09-_ ")', ":menù.09-_")
+        self.check_value('xs:NMTOKEN(xs:untypedAtomic(" :menù.09-_ "))', ":menù.09-_")
         self.wrong_value('xs:NMTOKEN("alpha+")')
         self.wrong_value('xs:NMTOKEN("hello world")')
         self.check_value('xs:NMTOKEN(())', [])
@@ -82,6 +85,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
 
     def test_name_constructor(self):
         self.check_value('xs:Name(" :base ")', ":base")
+        self.check_value('xs:Name(xs:untypedAtomic(" :base "))', ":base")
         self.check_value('xs:Name(" ::level_alpha ")', "::level_alpha")
         self.check_value('xs:Name("level-alpha")', "level-alpha")
         self.check_value('xs:Name("level.alpha\t\n")', "level.alpha")
@@ -98,6 +102,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
 
     def test_ncname_constructor(self):
         self.check_value('xs:NCName(" base ")', "base")
+        self.check_value('xs:NCName(xs:untypedAtomic(" base "))', "base")
         self.check_value('xs:NCName(" _level_alpha ")', "_level_alpha")
         self.check_value('xs:NCName("level-alpha")', "level-alpha")
         self.check_value('xs:NCName("level.alpha\t\n")', "level.alpha")
@@ -116,16 +121,20 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
 
     def test_id_constructor(self):
         self.check_value('xs:ID("xyz")', 'xyz')
+        self.check_value('xs:ID(xs:untypedAtomic("xyz"))', 'xyz')
 
     def test_idref_constructor(self):
         self.check_value('xs:IDREF("xyz")', 'xyz')
+        self.check_value('xs:IDREF(xs:untypedAtomic("xyz"))', 'xyz')
 
     def test_entity_constructor(self):
         self.check_value('xs:ENTITY("xyz")', 'xyz')
+        self.check_value('xs:ENTITY(xs:untypedAtomic("xyz"))', 'xyz')
 
     def test_qname_constructor(self):
         self.check_value('xs:QName(())', [])
         self.check_value('xs:QName("xs:element")', 'xs:element')
+        self.check_value('xs:QName(xs:untypedAtomic("xs:element"))', 'xs:element')
         self.assertRaises(KeyError, self.parser.parse, 'xs:QName("xsd:element")')
         self.wrong_type('xs:QName(5)', 'FORG0006', "the argument has an invalid type")
         self.wrong_value('xs:QName("1")', 'FOCA0002', "the argument must be an xs:QName")
@@ -135,6 +144,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
         self.check_value('xs:anyURI("https://example.com")', 'https://example.com')
         self.check_value('xs:anyURI("mailto:info@example.com")', 'mailto:info@example.com')
         self.check_value('xs:anyURI("urn:example:com")', 'urn:example:com')
+        self.check_value('xs:anyURI(xs:untypedAtomic("urn:example:com"))', 'urn:example:com')
         self.check_value('xs:anyURI("../principi/libertà.html")', '../principi/libertà.html')
         self.check_value('xs:anyURI("../principi/libert%E0.html")', '../principi/libert%E0.html')
         self.check_value('xs:anyURI("../path/page.html#frag")', '../path/page.html#frag')
@@ -169,6 +179,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
     def test_integer_constructors(self):
         self.wrong_value('xs:integer("hello")', 'FORG0001')
         self.check_value('xs:integer("19")', 19)
+        self.check_value('xs:integer(xs:untypedAtomic("19"))', 19)
         self.check_value("xs:integer('-5')", -5)
 
         root = self.etree.XML('<root a="19"/>')
@@ -221,6 +232,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
         self.wrong_value('xs:decimal("hello")')
         self.check_value('xs:decimal("19")', 19)
         self.check_value('xs:decimal("19")', Decimal)
+        self.check_value('xs:decimal(xs:untypedAtomic("19"))', 19)
 
         root = self.etree.XML('<root a="10.3"/>')
         context = XPathContext(root)
@@ -229,6 +241,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
     def test_double_constructor(self):
         self.wrong_value('xs:double("world")')
         self.check_value('xs:double("39.09")', 39.09)
+        self.check_value('xs:double(xs:untypedAtomic("39.09"))', 39.09)
         self.check_value('xs:double(-5)', -5.0)
         self.check_value('xs:double(-5)', float)
 
@@ -240,6 +253,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
     def test_float_constructor(self):
         self.wrong_value('xs:float("..")')
         self.check_value('xs:float(25.05)', 25.05)
+        self.check_value('xs:float(xs:untypedAtomic(25.05))', 25.05)
         self.check_value('xs:float(-0.00001)', -0.00001)
         self.check_value('xs:float(0.00001)', float)
 
@@ -252,9 +266,10 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
         tz0 = None
         tz1 = Timezone(datetime.timedelta(hours=5, minutes=24))
         self.check_value('xs:dateTime(())', [])
-        self.check_value(
-            'xs:dateTime("1969-07-20T20:18:00")', DateTime(1969, 7, 20, 20, 18, tzinfo=tz0)
-        )
+        self.check_value('xs:dateTime("1969-07-20T20:18:00")',
+                         DateTime(1969, 7, 20, 20, 18, tzinfo=tz0))
+        self.check_value('xs:dateTime(xs:untypedAtomic("1969-07-20T20:18:00"))',
+                         DateTime(1969, 7, 20, 20, 18, tzinfo=tz0))
         self.check_value('xs:dateTime("2000-05-10T21:30:00+05:24")',
                          datetime.datetime(2000, 5, 10, hour=21, minute=30, tzinfo=tz1))
         self.check_value('xs:dateTime("1999-12-31T24:00:00")',
@@ -267,10 +282,23 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
         self.wrong_value('xs:dateTime("2000-05-10T21:13:0")')
         self.wrong_type('xs:dateTime(50)', 'FORG0006', '1st argument has an invalid type')
 
+        root = self.etree.XML('<root a="1969-07-20T20:18:00"/>')
+        context = XPathContext(root)
+        self.check_value('xs:dateTime(@a)', DateTime(1969, 7, 20, 20, 18), context=context)
+
+        context.item = AttributeNode('a', DateTime(1969, 7, 20, 20, 18))
+        self.check_value('xs:dateTime(.)', DateTime(1969, 7, 20, 20, 18), context=context)
+
+        context.item = AttributeNode('a', True)
+        self.check_value('xs:dateTime(.)', TypeError, context=context)
+
     def test_time_constructor(self):
         tz0 = None
         tz1 = Timezone(datetime.timedelta(hours=5, minutes=24))
-        self.check_value('xs:time("21:30:00")', datetime.datetime(2000, 1, 1, 21, 30, tzinfo=tz0))
+        self.check_value('xs:time("21:30:00")',
+                         datetime.datetime(2000, 1, 1, 21, 30, tzinfo=tz0))
+        self.check_value('xs:time(xs:untypedAtomic("21:30:00"))',
+                         datetime.datetime(2000, 1, 1, 21, 30, tzinfo=tz0))
         self.check_value('xs:time("11:15:48+05:24")',
                          datetime.datetime(2000, 1, 1, 11, 15, 48, tzinfo=tz1))
 
@@ -285,8 +313,12 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
         tz0 = None
         tz2 = Timezone(datetime.timedelta(hours=-14, minutes=0))
 
-        self.check_value('xs:date("2017-01-19")', datetime.datetime(2017, 1, 19, tzinfo=tz0))
-        self.check_value('xs:date("2011-11-11-14:00")', datetime.datetime(2011, 11, 11, tzinfo=tz2))
+        self.check_value('xs:date("2017-01-19")',
+                         datetime.datetime(2017, 1, 19, tzinfo=tz0))
+        self.check_value('xs:date(xs:untypedAtomic("2017-01-19"))',
+                         datetime.datetime(2017, 1, 19, tzinfo=tz0))
+        self.check_value('xs:date("2011-11-11-14:00")',
+                         datetime.datetime(2011, 11, 11, tzinfo=tz2))
         self.wrong_value('xs:date("2011-11-11-14:01")')
         self.wrong_value('xs:date("11-11-11")')
         self.check_value('xs:date(())', [])
@@ -305,8 +337,12 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
         tz0 = None
         tz1 = Timezone(datetime.timedelta(hours=5, minutes=24))
 
-        self.check_value('xs:gDay("---30")', datetime.datetime(2000, 1, 30, tzinfo=tz0))
-        self.check_value('xs:gDay("---21+05:24")', datetime.datetime(2000, 1, 21, tzinfo=tz1))
+        self.check_value('xs:gDay("---30")',
+                         datetime.datetime(2000, 1, 30, tzinfo=tz0))
+        self.check_value('xs:gDay(xs:untypedAtomic("---30"))',
+                         datetime.datetime(2000, 1, 30, tzinfo=tz0))
+        self.check_value('xs:gDay("---21+05:24")',
+                         datetime.datetime(2000, 1, 21, tzinfo=tz1))
         self.wrong_value('xs:gDay("---32")')
         self.wrong_value('xs:gDay("--19")')
 
@@ -319,6 +355,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
 
     def test_gregorian_month_constructor(self):
         self.check_value('xs:gMonth("--09")', datetime.datetime(2000, 9, 1))
+        self.check_value('xs:gMonth(xs:untypedAtomic("--09"))', datetime.datetime(2000, 9, 1))
         self.check_value('xs:gMonth("--12")', datetime.datetime(2000, 12, 1))
         self.wrong_value('xs:gMonth("--9")')
         self.wrong_value('xs:gMonth("-09")')
@@ -335,8 +372,12 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
         tz0 = None
         tz2 = Timezone(datetime.timedelta(hours=-14, minutes=0))
 
-        self.check_value('xs:gMonthDay("--07-02")', datetime.datetime(2000, 7, 2, tzinfo=tz0))
-        self.check_value('xs:gMonthDay("--07-02-14:00")', datetime.datetime(2000, 7, 2, tzinfo=tz2))
+        self.check_value('xs:gMonthDay("--07-02")',
+                         datetime.datetime(2000, 7, 2, tzinfo=tz0))
+        self.check_value('xs:gMonthDay(xs:untypedAtomic("--07-02"))',
+                         datetime.datetime(2000, 7, 2, tzinfo=tz0))
+        self.check_value('xs:gMonthDay("--07-02-14:00")',
+                         datetime.datetime(2000, 7, 2, tzinfo=tz2))
         self.wrong_value('xs:gMonthDay("--7-02")')
         self.wrong_value('xs:gMonthDay("-07-02")')
         self.wrong_value('xs:gMonthDay("--07-32")')
@@ -350,6 +391,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
 
     def test_gregorian_year_constructor(self):
         self.check_value('xs:gYear("2004")', datetime.datetime(2004, 1, 1))
+        self.check_value('xs:gYear(xs:untypedAtomic("2004"))', datetime.datetime(2004, 1, 1))
         self.check_value('xs:gYear("-2004")', GregorianYear10(-2004))
         self.check_value('xs:gYear("-12540")', GregorianYear10(-12540))
         self.check_value('xs:gYear("12540")', GregorianYear10(12540))
@@ -365,7 +407,10 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
         self.check_value('xs:gYear(.)', XPathGregorianYear(1492), context=context)
 
     def test_gregorian_year_month_constructor(self):
-        self.check_value('xs:gYearMonth("2004-02")', datetime.datetime(2004, 2, 1))
+        self.check_value('xs:gYearMonth("2004-02")',
+                         datetime.datetime(2004, 2, 1))
+        self.check_value('xs:gYearMonth(xs:untypedAtomic("2004-02"))',
+                         datetime.datetime(2004, 2, 1))
         self.wrong_value('xs:gYearMonth("2004-2")')
         self.wrong_value('xs:gYearMonth("204-02")')
 
@@ -378,6 +423,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
 
     def test_duration_constructor(self):
         self.check_value('xs:duration("P3Y5M1D")', (41, 86400))
+        self.check_value('xs:duration(xs:untypedAtomic("P3Y5M1D"))', (41, 86400))
         self.check_value('xs:duration("P3Y5M1DT1H")', (41, 90000))
         self.check_value('xs:duration("P3Y5M1DT1H3M2.01S")', (41, Decimal('90182.01')))
         self.wrong_value('xs:duration("P3Y5M1X")')
@@ -392,6 +438,7 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
 
     def test_year_month_duration_constructor(self):
         self.check_value('xs:yearMonthDuration("P3Y5M")', (41, 0))
+        self.check_value('xs:yearMonthDuration(xs:untypedAtomic("P3Y5M"))', (41, 0))
         self.check_value('xs:yearMonthDuration("-P15M")', (-15, 0))
         self.check_value('xs:yearMonthDuration("-P20Y18M")',
                          YearMonthDuration.fromstring("-P21Y6M"))
@@ -410,6 +457,8 @@ class XPath2ConstructorsTest(xpath_test_class.XPathTestCase):
         self.check_value('xs:dayTimeDuration("-P2DT15H")', DayTimeDuration(seconds=-226800))
         self.check_value('xs:dayTimeDuration("PT240H")', DayTimeDuration.fromstring("P10D"))
         self.check_value('xs:dayTimeDuration("P365D")', DayTimeDuration.fromstring("P365D"))
+        self.check_value('xs:dayTimeDuration(xs:untypedAtomic("PT240H"))',
+                         DayTimeDuration.fromstring("P10D"))
         self.check_value('xs:dayTimeDuration("-P2DT15H0M0S")',
                          DayTimeDuration.fromstring('-P2DT15H'))
         self.check_value('xs:dayTimeDuration("P3DT10H")', DayTimeDuration.fromstring("P3DT10H"))
