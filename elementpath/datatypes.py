e6323332a7b61a4f893be8c5b4c6fbb4246d0ed6
Jelte Jansen <github@tjeb.nl>
2020-04-13 13:41:03 +0200

    Explicitely check for values of xs:boolean in UntypedAtomic boolean context
    This solves an issue when the value of an xs:boolean is used in selectors (e.g. //books[available=true()])

--- elementpath/datatypes.py
+++ elementpath/datatypes.py
@@ -983,7 +983,12 @@ class UntypedAtomic(object):
         # isinstance(boolean_variable, int) returns true, since boolean is
         # a subclass of int; it is therefore necessary to check for bool first
         elif isinstance(other, bool):
-            return bool(self.value), other
+            # Extra check: We may have an Untyped here which need to be considered an
+            # xs:boolean; check if that is the case by checking its value
+            if isinstance(self, UntypedAtomic) and self.value in ['0', '1', 'true', 'false']:
+                return self.value in ['1', 'true'], other
+            else:
+                return bool(self.value), other
         elif isinstance(other, int):
             return float(self.value), other
         else:
