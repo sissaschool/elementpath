2d14b9fde23ef74379d1209d3f683dad1ca2004b
Davide Brunato <brunato@sissa.it>
2021-01-06 18:19:41 +0100

    Update release and copyright info

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ author = 'Davide Brunato'
 # The short X.Y version
 version = '2.1'
 # The full version, including alpha/beta/rc tags
-release = '2.1.0'
+release = '2.1.1'
 
 
 # -- General configuration ---------------------------------------------------
